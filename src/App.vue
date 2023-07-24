e3cdb722e21826de81f81c1ba9c74f0b93fc5c29
Natsuki Okada <coconana.9042@gmail.com>
2023-07-24 22:11:06 +0900

    WIP html

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,33 @@
 <script setup lang="ts"></script>
 
 <template>
-  <header>
-    <!-- App.vue is the starting place for our vue app. In Index.html you can see a div with an id of app
-    that div will display the contents of this App.vue file! -->
-    <h2>Lets get started and look at App.vue!</h2>
-    <div class="wrapper">
-      <!-- Here we are calling the HelloWorld.Vue component and setting the msg attribute for it! -->
-      <!-- setting the "msg" attribute here is setting the prop in HelloWorld.vue to be "You did it!" -->
-      <HelloWorld msg="You did it! (This is passed through to the HelloWorld component from App.vue!)" />
+  <article>
 
-      <!-- This is how we can create paging in our app
-      See how these are setup in "src/router/index.ts"
-      Using the RouterLink element will change the content in the RouterView to be whatever is in that view -->
-      
+    <div id="mainimage">
+      <img src="images/drawers.jpg" alt="A green drawer">
     </div>
-  </header>
-  
-  <!-- Anything contained in a RouterView will change depending on the current route (Url) that you are on! -->
-  <!-- If you click on About, it will take you to /about.  -->
-  <!-- See how the AboutView.vue html is displayed here -->
-  <!-- It will basically replace this <RouterView /> line with   
-  <div class="about">
-    <h3>This is AboutView.vue !</h3>
-  </div> -->
-  
+
+    <section id="headline" >
+      <h2 class="article__headline">Shift the overall look and feel by adding these wonderful touches to furniture in your home</h2>
+    </section>
+
+    <section id="maintext" >
+      <p>Ever been in a room and felt like something was missing? Perhaps it felt slightly bare and uninviting. I've got some simple tips to help you make any room feel complete.</p>
+    </section>
+
+    <section id="profile">
+      <div class="article__profile">
+        <img src="images/avatar-michelle.jpg" alt="avatar-michelle">
+        <div class="about">
+          <h3 class="name">Michelle Appleton</h3>
+          <p>28 Jun 2020</p>
+        </div>
+      </div>
+    </section>
+
+  </article>
 </template>
 
 <style scoped>
-
-a {
-  padding-right: 2rem;;
-}
 
 </style>
